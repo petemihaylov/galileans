5f50dc64ff4c84949ebc7aa94f9591a0f2b22a87
Kevin Westerweele <391065@student.fontys.nl>
2020-03-02 15:37:13 +0100

    UML Models and Database connection
    Finished the UML Models for now and made the connection string connect to the right database

--- desktop/EmployeesManagementSystem/Models/User.cs
+++ desktop/EmployeesManagementSystem/Models/User.cs
@@ -10,16 +10,14 @@ namespace EmployeesManagementSystem.Models
     public class User
     {
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
         public User() { }
-        public User(string firstName, string lastName, string email, string password, string role)
+        public User(string fullName, string email, string password, string role)
         {
-            this.FirstName = firstName;
-            this.LastName = lastName;
+            this.FullName = fullName;
             this.Email = email;
             this.Role = role;
             this.Password = password;
