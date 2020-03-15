b9a15c6d42ec43f99e6953d31895f30993c253c1
Ioan,Alexandra A <alexandra.ioan@student.fontys.nl>
2020-03-16 00:05:39 +0100

    edit stats
    the attendance per employee

--- desktop/EmployeesManagementSystem/Statistic.cs
+++ desktop/EmployeesManagementSystem/Statistic.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
 using EmployeesManagementSystem.Models;
-
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace EmployeesManagementSystem
 {
@@ -17,14 +17,13 @@ namespace EmployeesManagementSystem
         private int hoursWorked, hrs, id;
         private int hoursSkipped;
         int counter = 0;
-        bool worked = false;
-
 
         // Keeps  track of the current loggedUser
         private User loggedUser;
 
         float money = 0;
         DateTime today = DateTime.Today;
+        //keeps track of the month at the center
         DateTime keepTrack = DateTime.Today;
 
 
@@ -61,78 +60,127 @@ namespace EmployeesManagementSystem
             }
         }
 
-        private void UpdateStatisticsEmployee()
+        private void AttendancePerEmployee()
         {
-            shifts = databaseContext.GetShiftsByID(Convert.ToInt32(cbEmployee.Text)); // try catch 
+            try
+            {
+                shifts = databaseContext.GetShiftsByID(Convert.ToInt32(cbEmployee.Text)); // try catch
+            }
+            catch (Exception)
+            {
+                throw new Exception("Can't take employee's shifts");
+            }
+
             User user = databaseContext.GetUserByID(Convert.ToInt32(cbEmployee.Text));
 
-            if (keepTrack.ToString("MMM").ToUpper() == monthCenter.Text && keepTrack.ToString("yyyy") == yearCenter.Text) 
+            //clears the chart
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+
+            //attendance per employee
+            foreach (Shift shift in shifts)
+            {
+                if (keepTrack.ToString("MMM").ToUpper() == monthCenter.Text && keepTrack.ToString("yyyy") == yearCenter.Text)
+                {
+                    //marks attended
+                    if (shift.Attended == true)
+                        chart1.Series["Present"].Points.Add(new DataPoint() { AxisLabel = Convert.ToString(shift.ShiftDate.Day) + "/" + Convert.ToString(shift.ShiftDate.Month), XValue = 31 * shift.ShiftDate.Month + shift.ShiftDate.Day, YValues = new double[] { shift.StartTime.Hour + shift.StartTime.Minute / 60, shift.EndTime.Hour + shift.EndTime.Minute / 60 } });
+                    //marks absent
+                    else if (shift.Attended == false && shift.ShiftDate < today)
+                        chart1.Series["Absent"].Points.Add(new DataPoint() { AxisLabel = Convert.ToString(shift.ShiftDate.Day) + "/" + Convert.ToString(shift.ShiftDate.Month), XValue = 31 * shift.ShiftDate.Month + shift.ShiftDate.Day, YValues = new double[] { shift.StartTime.Hour + shift.StartTime.Minute / 60, shift.EndTime.Hour + shift.EndTime.Minute / 60 } });
+                    //marks future scheduled shifts
+                    else if (shift.Attended == false && shift.ShiftDate > today)
+                    {
+                        chart1.Series["Scheduled"].Points.Add(new DataPoint() { AxisLabel = Convert.ToString(shift.ShiftDate.Day) + "/" + Convert.ToString(shift.ShiftDate.Month), XValue = 31 * shift.ShiftDate.Month + shift.ShiftDate.Day, YValues = new double[] { shift.StartTime.Hour + shift.StartTime.Minute / 60, shift.EndTime.Hour + shift.EndTime.Minute / 60 } });
+                    }
+                }
+            }
+        }
+
+        private void AttendancePerDepartment()
+        {
+            //clears the chart
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+        }
+
+        private void WagePerEmployee()
+        {
+            try
+            {
+                shifts = databaseContext.GetShiftsByID(Convert.ToInt32(cbEmployee.Text)); // try catch
+            }
+            catch (Exception)
+            {
+                throw new Exception("Can't take employee's shifts");
+            }
+            User user = databaseContext.GetUserByID(Convert.ToInt32(cbEmployee.Text));
+
+            //clears the chart
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+
+            //if the month selected is the one in center
+            if (keepTrack.ToString("MMM").ToUpper() == monthCenter.Text && keepTrack.ToString("yyyy") == yearCenter.Text)
             {
                 foreach (Shift shift in shifts)
                 {
-                    worked = false;
                     money = 0;
-
-                    //to add  && DateTime.Compare(keepTrack,shift.ShiftDate) > 0
-                    if (keepTrack.Month == shift.ShiftDate.Month && keepTrack.Day > shift.ShiftDate.Day)
+                    if (shift.Attended)
                     {
-                        money = (shift.EndTime - shift.StartTime).Hours * user.HourlyRate;
-                        if (money > 0)
+                        //for all shifts that took place during the selected month, before todays date (in case of current month)
+                        if (keepTrack.Month == shift.ShiftDate.Month && keepTrack.Day > shift.ShiftDate.Day)
                         {
-                            if (!shift.Attended)
-                            {
-                                hoursSkipped++;
-                                worked = true;
-                            }
-                            else
-                            {
-                                hrs = shift.GetInfo();
-                                hoursWorked += hrs;
-                            }
+                            //money = hrsWorked * wage
+                            money = (shift.EndTime - shift.StartTime).Hours * user.HourlyRate;
+                            hrs = shift.GetInfo();
+                            hoursWorked += hrs;
+                        }
+                    }
+                    else
+                    {
+                        if (keepTrack.Month == shift.ShiftDate.Month && keepTrack.Day > shift.ShiftDate.Day)
+                        {
+                            hoursSkipped++;
                         }
                     }
                 }
+            }
+            /*
+            chart1.Series["Series1"].Points.AddXY(hoursWorked * 100 / (hoursWorked + hoursSkipped) + "%", hoursWorked);
+            chart1.Series["Series1"].Points.AddXY(hoursSkipped * 100 / (hoursWorked + hoursSkipped) + "%", hoursSkipped);
+            */
+            /*foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+            chart1.Series["Series1"].Points.AddXY(hoursWorked, money); ;
+            lbMoneyMadeMonth.Text = Convert.ToString(money) + "$";*/
+        }
 
-                foreach (var series in chart1.Series)
-                {
-                    series.Points.Clear();
-                }
-                foreach (Shift shift in shifts)
-                {
-                    if (shift.Attended == true)
-                        chart1.Series["Present"].Points.AddXY(shift.ShiftDate.Day + "/" + shift.ShiftDate.Month, shift.StartTime);
-                    else
-                        chart1.Series["Absent"].Points.AddXY(shift.ShiftDate.Day + "/" + shift.ShiftDate.Month, shift.StartTime);
-                    if (shift.ShiftDate > today)
-                    {
-                        chart1.Series["Scheduled"].Points.AddXY(shift.ShiftDate.Day + "/" + shift.ShiftDate.Month, shift.StartTime);
-                    }
-                }
-
-                //chart1.Series["Series1"].Points.AddXY(hoursWorked * 100 / (hoursWorked + hoursSkipped) + "%", hoursWorked);
-                //chart1.Series["Series1"].Points.AddXY(hoursSkipped * 100 / (hoursWorked + hoursSkipped) + "%", hoursSkipped);
-
-                /*foreach (var series in chart1.Series)
-                {
-                    series.Points.Clear();
-                }
-                chart1.Series["Series1"].Points.AddXY(hoursWorked, money); ;
-                lbMoneyMadeMonth.Text = Convert.ToString(money) + "$";*/
+        private void Turnover()
+        {
+            //clears the chart
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
             }
 
         }
 
-        public void UpdateStatisticDepartment()
-        {
-
-        }
-
+    
         private void btnToday_Click(object sender, EventArgs e)
         {
             counter = 0;
             keepTrack = today;
 
-            monthLeft.Text = today.ToString("MMM").ToUpper();
+            monthLeft.Text = today.AddMonths(counter - 1).ToString("MMM").ToUpper();
             monthCenter.Text = today.ToString("MMM").ToUpper();
             monthRight.Text = today.AddMonths(counter + 1).ToString("MMM").ToUpper();
 
@@ -140,6 +188,17 @@ namespace EmployeesManagementSystem
             yearCenter.Text = today.ToString("yyyy");
             yearRight.Text = today.ToString("yyyy");
 
+            if (cbStatistic.Text == "Attendance per employee")
+                AttendancePerEmployee();
+
+            else if (cbStatistic.Text == "Attendance per department")
+                AttendancePerDepartment();
+
+            else if (cbStatistic.Text == "Turnover")
+                Turnover();
+
+            else if (cbStatistic.Text == "Wage per employee")
+                WagePerEmployee();
         }
 
         private void arrowLeft_Click(object sender, EventArgs e)
@@ -155,11 +214,17 @@ namespace EmployeesManagementSystem
             yearCenter.Text = today.AddMonths(counter).ToString("yyyy");
             yearRight.Text = today.AddMonths(counter + 1).ToString("yyyy");
 
-            if (cbStatistic.Text.Contains("employee"))
-                UpdateStatisticsEmployee();
-            else if (cbStatistic.Text.Contains("department"))
-                UpdateStatisticDepartment();
+            if (cbStatistic.Text == "Attendance per employee")
+                AttendancePerEmployee();
 
+            else if (cbStatistic.Text == "Attendance per department")
+                AttendancePerDepartment();
+
+            else if (cbStatistic.Text == "Turnover")
+                Turnover();
+
+            else if (cbStatistic.Text == "Wage per employee")
+                WagePerEmployee();
         }
 
         private void arrowRight_Click(object sender, EventArgs e)
@@ -174,10 +239,17 @@ namespace EmployeesManagementSystem
             yearCenter.Text = today.AddMonths(counter).ToString("yyyy");
             yearRight.Text = today.AddMonths(counter + 1).ToString("yyyy");
 
-            if (cbStatistic.Text.Contains("employee"))
-                UpdateStatisticsEmployee();
-            else if (cbStatistic.Text.Contains("department"))
-                UpdateStatisticDepartment();
+            if (cbStatistic.Text == "Attendance per employee")
+                AttendancePerEmployee();
+
+            else if (cbStatistic.Text == "Attendance per department")
+                AttendancePerDepartment();
+
+            else if (cbStatistic.Text == "Turnover")
+                Turnover();
+
+            else if (cbStatistic.Text == "Wage per employee")
+                WagePerEmployee();
 
         }
 
@@ -202,6 +274,7 @@ namespace EmployeesManagementSystem
                 cbEmployee.Enabled = true;
                 cbEmployee.Show();
                 lbEmployee.Show();
+                
                 foreach (User item in users)
                 {
                     cbEmployee.Items.Add(item.ID);
@@ -219,10 +292,141 @@ namespace EmployeesManagementSystem
                 }
 
             }
+
             else
             {
                 cbEmployee.Hide();
                 lbEmployee.Hide();
+            }
+
+            if (cbStatistic.Text.Contains("Attendance"))
+            {
+                //labels for y axis hours
+                CustomLabel label1 = new CustomLabel
+                {
+                    FromPosition = 8.5,
+                    ToPosition = 9.5,
+                    Text = "09:00"
+                };
+
+                CustomLabel label2 = new CustomLabel
+                {
+                    FromPosition = 9.5,
+                    ToPosition = 10.5,
+                    Text = "10:00"
+                };
+
+                CustomLabel label3 = new CustomLabel
+                {
+                    FromPosition = 10.5,
+                    ToPosition = 11.5,
+                    Text = "11:00"
+                };
+
+                CustomLabel label4 = new CustomLabel
+                {
+                    FromPosition = 11.5,
+                    ToPosition = 12.5,
+                    Text = "12:00"
+                };
+
+                CustomLabel label5 = new CustomLabel
+                {
+                    FromPosition = 12.5,
+                    ToPosition = 13.5,
+                    Text = "13:00"
+                };
+
+                CustomLabel label6 = new CustomLabel
+                {
+                    FromPosition = 13.5,
+                    ToPosition = 14.5,
+                    Text = "14:00"
+                };
+
+                CustomLabel label7 = new CustomLabel
+                {
+                    FromPosition = 14.5,
+                    ToPosition = 15.5,
+                    Text = "15:00"
+                };
+
+                CustomLabel label8 = new CustomLabel
+                {
+                    FromPosition = 15.5,
+                    ToPosition = 16.5,
+                    Text = "16:00"
+                };
+
+                CustomLabel label9 = new CustomLabel
+                {
+                    FromPosition = 16.5,
+                    ToPosition = 17.5,
+                    Text = "17:00"
+                };
+
+                CustomLabel label10 = new CustomLabel
+                {
+                    FromPosition = 17.5,
+                    ToPosition = 18.5,
+                    Text = "18:00"
+                };
+
+                CustomLabel label11 = new CustomLabel
+                {
+                    FromPosition = 18.5,
+                    ToPosition = 19.5,
+                    Text = "19:00"
+                };
+
+                CustomLabel label12 = new CustomLabel
+                {
+                    FromPosition = 19.5,
+                    ToPosition = 20.5,
+                    Text = "20:00"
+                };
+
+                CustomLabel label13 = new CustomLabel
+                {
+                    FromPosition = 20.5,
+                    ToPosition = 21.5,
+                    Text = "21:00"
+                };
+
+                CustomLabel label14 = new CustomLabel
+                {
+                    FromPosition = 21.5,
+                    ToPosition = 22.5,
+                    Text = "22:00"
+                };
+
+                CustomLabel label15 = new CustomLabel
+                {
+                    FromPosition = 22.5,
+                    ToPosition = 23.5,
+                    Text = "23:00"
+                };
+
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label1);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label2);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label3);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label4);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label5);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label6);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label7);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label8);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label9);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label10);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label11);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label12);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label13);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label14);
+                chart1.ChartAreas[0].AxisY.CustomLabels.Add(label15);
+
+            }
+            else
+            {
+                chart1.ChartAreas[0].AxisY.CustomLabels.Clear();
             }
         }
 
@@ -231,12 +435,23 @@ namespace EmployeesManagementSystem
 
             cbEmployee.Hide();
             lbEmployee.Hide();
+            
         }
 
         private void cbEmployee_SelectedIndexChanged(object sender, EventArgs e)
         {
 
-            UpdateStatisticsEmployee();
+            if (cbStatistic.Text == "Attendance per employee")
+                AttendancePerEmployee();
+
+            else if (cbStatistic.Text == "Attendance per department")
+                AttendancePerDepartment();
+
+            else if (cbStatistic.Text == "Turnover")
+                Turnover();
+
+            else if (cbStatistic.Text == "Wage per employee")
+                WagePerEmployee();
         }
     }
 }
