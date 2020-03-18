d9b8473e06f6626f343799cdeaab5dcca7e7bf9e
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-03-18 23:33:24 +0100

    add

--- web/shared/header.php
+++ web/shared/header.php
@@ -5,16 +5,16 @@
   <div class="collapse navbar-collapse">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="welcome.php"><i class="fas fa-home "></i> Shifts</a>
+        <a class="nav-link" href="welcome.php"><i class="fas fa-home "></i> My Shifts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="preferences.php"> <i class="fas fa-praying-hands"></i></i> Preferences</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="details.php"><i class="fas fa-info-circle faa-spin animated-hover"></i> Details</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="notify.php">  <i class="far fa-envelope "></i> Notifications</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="preferences.php"> <i class="fas fa-praying-hands"></i></i> Preferences</a>
+        <a class="nav-link" href="notify.php">  <i class="far fa-envelope "></i> Cancel</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="payments.php"> <i class="far fa-money-bill-alt"></i> Payments</a>
