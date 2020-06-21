1be9d0f95ded320cc078574604b213b8f081342c
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-06-21 17:48:24 +0200

    Adding Notification Context Controller and Model

---
+++ desktop/EmployeesManagementSystem/Data/NotificationContext.cs
@@ -0,0 +1,35 @@
+﻿using EmployeesManagementSystem.Models;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace EmployeesManagementSystem.Data
+{
+    public class NotificationContext : DbContext
+    {
+        public override bool DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Insert(object obj)
+        {
+            Notification notification = (Notification)obj;
+
+            using (var con = new MySqlConnection(connectionString))
+            {
+                using (var command = con.CreateCommand())
+                {
+                    con.Open();
+
+                    command.CommandText = @"INSERT INTO Notification (Message, UserID) VALUES(@message, @userId);";
+
+                    command.AddParameter("message", notification.Message);
+                    command.AddParameter("userId", notification.User.ID);
+ 
+                    return command.ExecuteNonQuery() > 0 ? true : false;
+
+                }
+            }
+        }
+    }
+}
---
+++ desktop/EmployeesManagementSystem/Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagementSystem.Models
+{
+    class Notification
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+
+        private User user = new User();
+        public User User { get { return this.user; } set { this.user = value; } }
+
+        public Notification(string message, User user)
+        {
+            this.Message = message;
+            this.User = user;
+        }
+    }
+}
