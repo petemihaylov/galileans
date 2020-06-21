a121a4a0e5c5881eaf037ae0059189b43b33990b
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-06-21 20:03:06 +0200

    Notification model WEB

---
+++ web/models/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+class Shift{
+
+    private $ID;
+    private $message;
+    private $userID;
+
+
+    function __construct($id, $message, $userID){
+        $this->ID = $id;
+        $this->message = $message;
+        $this->userID = $userID; 
+    }
+
+    function get_ID(){
+        return $this->ID;
+    }
+
+    function get_Message(){
+        return $this->message;
+    }
+
+    function get_UserID(){
+        return $this->userID;
+    }
+}
+?>
