5f6758a60bb2fd5b736e02ce579599ae5a1c426f
Konstantin Mihaylov <mihailovkonstantin364@gmail.com>
2020-06-22 18:29:23 +0200

    Add to Models Interfaces

---
+++ desktop/EmployeesManagementSystem/Models/Department/IDepartment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesManagementSystem.Models.Department
+{
+    interface IDepartment
+    {
+        //TO-DO
+    }
+}
